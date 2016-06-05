3b13028f3a3f9c0b542b994f86576c966b60bec0
Agradeep Khanra <agradeep92@gmail.com>
2016-06-04 19:33:02 -0500

    More CSS changes

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 /*var Examples = React.createClass({
 	render: function() {
@@ -11,8 +12,16 @@ var React = require('react');
 var Examples = (props) => {
 	return (
 		<div>
-			<h2>Examples</h2>
-			<p>Examples coming soon...</p>
+			<h1 className="text-center">Examples</h1>
+			<p>Here are a few example locations to try out:</p>
+			<ol>
+				<li>
+					<Link to='/?location=chicago'>Chicago, IL</Link>
+				</li>
+				<li>
+					<Link to='/?location=london'>London, UK</Link>
+				</li>
+			</ol>
 		</div>
 	);
 }
